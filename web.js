c8813c466bd6bd39822128a218be92c992c42e04
vagrant <vagrant@precise32.(none)>
2013-07-06 21:24:38 +0000

    Added node.js Buffers.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+     var output = Buffer(fs.readFileSync('index.html'));
+     response.send(output.toString());
 });
 
 var port = process.env.PORT || 5000;
